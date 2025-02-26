0d4f904c4879a0c3f90c3817d667fc77870c0df0
wcozendey <138802363+wcozendey@users.noreply.github.com>
2025-02-26 16:47:11 +0000

    Delete agendapri directory

--- agendapri/app_agenda/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppAgendaConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "app_agenda"
--- agendapri/app_services/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppServicesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "app_services"
